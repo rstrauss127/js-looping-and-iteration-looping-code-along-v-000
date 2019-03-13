6bca2670a7a5781cd7c25cff1742276789343b26
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-13 21:47:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function printBadges(array) {
+  for (var i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  let sum = 0;
+  while (Math.random() >= 0.5) {
+    sum++;
+  }
+  return `You got ${sum} tails in a row!`;
+}

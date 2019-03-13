95f71f179a8c45d36b68462ad9e61a87d8f92a95
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-13 21:38:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function printBadges(a) {
   for (let i = 0; i < a.length; i++) {
     console.log(`Welcome ${a[i]}! You are employee #${i}.`);
   }
-  return array;
+  return a;
 }
 
 function maybeTrue() {
